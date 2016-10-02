ec4a10da6cddc6f3ba515c19fe17ebf10cc6b2fc
dviramontes <dviramontes@gmail.com>
2016-10-02 14:07:31 -0700

    use -t or -w when OS is linux or mac os

--- src/clj/owlet_status/routes/api.clj
+++ src/clj/owlet_status/routes/api.clj
@@ -4,12 +4,21 @@
             [compojure.api.sweet :refer [context]])
   (use [clojure.java.shell :only [sh]]))
 
+(defn get-flag
+  "depending on OS"
+  []
+  (let [os (System/getProperty "os.name")]
+    (case os
+      "Linux"    "-w"
+      "Mac OS X" "-t"
+      :default   "-w")))
+
 (defn timed-ping
   "Time a .isReachable ping to a given domain"
   [domain & [timeout]]
   (let [t (str (or timeout 5))
         start (. System (nanoTime))
-        ping! (sh "ping" "-c" "1" "-t" t domain)
+        ping! (sh "ping" "-c" "1" (get-flag) t domain)
         total (/ (double (- (. System (nanoTime)) start)) 1000000.0)]
     {:time total :result ping!}))
 
@@ -18,8 +27,7 @@
   "Note timeout param is in seconds"
   (let [domain (get-in req [:route-params :domain])
         {:keys [time result]} (timed-ping domain)]
-        ;{:keys [exit out err]} result]
-    (ok {:time time
+    (ok {:time   time
          :result result})))
 
 (defroutes api-routes
